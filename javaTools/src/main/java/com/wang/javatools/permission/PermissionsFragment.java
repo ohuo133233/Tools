dee17f1f5b6b3aebbda8d68557ec3a2325085d20
wangzhifeng <1441289824@qq.com>
2022-04-15 10:27:53 +0800

    获取权限的null判断兼容

--- javaTools/src/main/java/com/wang/javatools/permission/PermissionsFragment.java
+++ javaTools/src/main/java/com/wang/javatools/permission/PermissionsFragment.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-
 public class PermissionsFragment extends Fragment {
     public static final String TAG = PermissionsFragment.class.getSimpleName();
     private IPermissionCallBack mIPermissionCallBack;
@@ -41,14 +40,17 @@ public class PermissionsFragment extends Fragment {
             } else {
                 // 如果没有获取权限，那么可以提示用户去设置界面--->应用权限开启权限
                 Log.d(TAG, "获取权限失败");
-
                 // 判断用户是否 点击了不再提醒。(检测该权限是否还可以申请)
                 if (shouldShowRequestPermissionRationale(permissions[0])) {
                     Log.d(TAG, "用户点击了不再提醒");
-                    mIPermissionCallBack.noMoreReminders();
+                    if (mIPermissionCallBack != null) {
+                        mIPermissionCallBack.noMoreReminders();
+                    }
                 } else {
                     Log.d(TAG, "用户没有点击不再提醒");
-                    mIPermissionCallBack.fail();
+                    if (mIPermissionCallBack != null) {
+                        mIPermissionCallBack.fail();
+                    }
                 }
             }
         }
